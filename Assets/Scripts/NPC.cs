9c2a2277a327ec86dd0763c71d575a344de35dcf
Jengstrom124 <87103784+Jengstrom124@users.noreply.github.com>
2026-04-12 16:23:11 +1000

    Env Assets + DIA Variants Implemented

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net.Security;
 using UnityEngine;
+using DG.Tweening;
 
 public class NPC : MonoBehaviour
 {
+    [Header("Main Setup")]
     [SerializeField] bool forceBad = false;
     [SerializeField] GameObject goodDialogue, badDialogue;
     [SerializeField] AudioClip startSFX, deathSFX;
+
+    [Header("Death Tween")]
+    [SerializeField] float spinDuration = 1f;
+    [SerializeField] Vector3 deathJumpPos = new Vector3(100, 100, 0);
+    [SerializeField] float jumpPower = 5f, jumpDuration = 3f;
     //REF
     [SerializeField] bool isGood = true;
 
@@ -32,12 +39,17 @@ public class NPC : MonoBehaviour
     {
         if(startSFX == null) return;       
         AudioManager.instance.PlayOneShot(startSFX);
-        Debug.Log("Convo Started");
     }
 
+    [ContextMenu("Debug/Exterminate")]
     public void Exterminate()
     {
         AudioManager.instance.PlayOneShot(deathSFX);
+        transform.DORotate(new Vector3(0, 0, 1080f), spinDuration, RotateMode.FastBeyond360);
+        transform.DOJump(transform.position + deathJumpPos, jumpPower, 1, jumpDuration).SetEase(Ease.Linear).OnComplete(DisableGO);
+    }
+    void DisableGO()
+    {
         gameObject.SetActive(false);
     }
 }
